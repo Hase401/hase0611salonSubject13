e83a38ec14a01066d459da401b6e4d3ac8236e33
Hase401 <83523126+Hase401@users.noreply.github.com>
2021-06-11 17:27:51 +0900

    Add files via upload

---
+++ subject_13/subject_13/ViewController/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  subject_13
+//
+//  Created by 長谷川孝太 on 2021/06/09.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView!
+    private let keyCheckMark = "checkMark"
+    private let keyName = "name"
+    //　【メモ】private var fruits: [Dictionary<String, Any>] = []
+    private var fruits: [[String: Any]] = []
+    // 【SwiftLint行間エラー対策】
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.fruits = [
+            [keyCheckMark: false, keyName: "りんご"],
+            [keyCheckMark: true, keyName: "みかん"],
+            [keyCheckMark: false, keyName: "バナナ"],
+            [keyCheckMark: true, keyName: "パイナップル"]
+        ]
+        tableView.register(CustomTableViewCell.nib(), forCellReuseIdentifier: CustomTableViewCell.identifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        self.fruits.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 【疑問】このメソッドで"guard let -- else { return }"はできないのでダウンキャストas!を使うしか他に方法はないのか？
+        let customCell = tableView.dequeueReusableCell(
+                            // swiftlint:disable:next force_cast
+                            withIdentifier: CustomTableViewCell.identifier, for: indexPath) as! CustomTableViewCell
+        customCell.configure(with: (fruits[indexPath.row][keyName] as? String) ?? "",
+                             imageName: "checkmark",
+                             isCheckMark: (fruits[indexPath.row][keyCheckMark] as? Bool) ?? false)
+        return customCell
+    }
+}

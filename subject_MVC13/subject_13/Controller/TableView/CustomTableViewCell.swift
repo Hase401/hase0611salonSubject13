117f36e3e424b5dfab41cc4bd862b63b9b5f8f27
Hase401 <83523126+Hase401@users.noreply.github.com>
2021-06-12 11:16:29 +0900

    Add files via upload

---
+++ subject_MVC13/subject_13/Controller/TableView/CustomTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTableViewCell.swift
+//  subject_13
+//
+//  Created by 長谷川孝太 on 2021/06/11.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    static let identifier = "CustomTableViewCell"
+    static func nib() -> UINib {
+        return UINib(nibName: "CustomTableViewCell", bundle: nil)
+    }
+
+    public func configure(with title: String, imageName: String, isCheckMark: Bool) {
+        customLabel.text = title
+        if isCheckMark {
+            customImageView.image = UIImage(systemName: imageName)
+        } else { return }
+    }
+
+    @IBOutlet private var customLabel: UILabel!
+    @IBOutlet private var customImageView: UIImageView!
+}
